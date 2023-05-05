fc1966726c6ac0eb261f1791efd01dda1336ccc9
Kevin Andrews <andrewskevin92@gmail.com>
2023-05-04 19:00:05 -0700

    change type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { type AppType, AppProps } from "next/app";
+import type { AppType, AppProps } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
