a4ed32f5d4fa6c8adc0d19f86aefd16fd3e74db8
Kevin Andrews <andrewskevin92@gmail.com>
2023-05-04 22:09:02 -0700

    add school and columns section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import aboutKevin from "../../../public/images/AboutKevin.svg";
-import { FALSE } from "sass";
+import universityIcon from "/public/images/icons8-university-50.png";
 
 interface TextColorType {
   text: "string";
@@ -27,7 +26,7 @@ const About = () => {
         <div style={{ marginLeft: "1rem" }}>
           <p>
             {textColor("const", "blue")} {textColor("interest", "yellow")} =
-            prompt("Web Developer or Data Analyst");
+            prompt(&quot;Web Developer or Data Analyst&quot;);
           </p>
           <br />
           <div style={{ marginLeft: "1rem" }}>
@@ -43,7 +42,8 @@ const About = () => {
             <br />
             <br />
             <p>
-              {textColor("if", "green")} {"("}interest === "Web Developer"{")"}
+              {textColor("if", "green")} {"("}interest === &quot;Web
+              Developer&quot;{")"}
               {"  {"}
             </p>
             <div style={{ marginLeft: "3rem" }}>
@@ -63,7 +63,8 @@ const About = () => {
             <br />
             <p>
               {"} "}
-              {textColor("else if", "green")} {"("}interest === "Data Analyst"
+              {textColor("else if", "green")} {"("}interest === &quot;Data
+              Analyst&quot;
               {")"}
               {"  {"}
             </p>
@@ -88,6 +89,163 @@ const About = () => {
             <p>{textColor("return", "green")} about;</p>
 
             <div style={{ marginLeft: "-2rem" }}>{"};"}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="box row">
+        <div id="school" className="box small jetbrains">
+          <span className="icon">
+            <Image
+              src={universityIcon}
+              height={100}
+              width={100}
+              alt="university-icon"
+              className="icon"
+            />
+          </span>
+          <span className="flex-center">
+            <div className="indent" style={{ marginLeft: "2rem" }}>
+              <p>
+                {textColor("const", "blue")}
+                {" BS = "}
+                {textColor("Business Administration", "yellow")}
+                {";"}
+              </p>
+              <p>
+                {textColor("const", "blue")}
+                {" specialization = "}
+                {textColor("Marketing", "yellow")}
+                {";"}
+              </p>
+              <p>
+                {textColor("const", "blue")}
+                {" college = "}
+                {textColor("'Central Washington University'", "yellow")}
+                {";"}
+              </p>
+              <p>
+                {textColor("const", "blue")}
+                {" graduationYear = "}
+                {textColor("2016", "yellow")}
+                {";"}
+              </p>
+            </div>
+          </span>
+        </div>
+        <div id="school" className="box small jetbrains">
+          <span className="icon">
+            <Image
+              src={universityIcon}
+              height={100}
+              width={100}
+              alt="university-icon"
+              className="icon"
+            />
+          </span>
+          <span className="flex-center">
+            <div className="indent" style={{ marginLeft: "2rem" }}>
+              <p>
+                {textColor("const", "blue")}
+                {" BS = "}
+                {textColor("Business Administration", "yellow")}
+                {";"}
+              </p>
+              <p>
+                {textColor("const", "blue")}
+                {" specialization = "}
+                {textColor("Marketing", "yellow")}
+                {";"}
+              </p>
+              <p>
+                {textColor("const", "blue")}
+                {" college = "}
+                {textColor("'Central Washington University'", "yellow")}
+                {";"}
+              </p>
+              <p>
+                {textColor("const", "blue")}
+                {" graduationYear = "}
+                {textColor("2016", "yellow")}
+                {";"}
+              </p>
+            </div>
+          </span>
+        </div>
+        <div className="box small jetbrains">
+          <p>
+            {textColor("export", "purple")} {textColor("const", "blue")}{" "}
+            AboutKevin = () ={">"} {"{"}
+          </p>
+          <div style={{ marginLeft: "1rem" }}>
+            <p>
+              {textColor("const", "blue")} {textColor("interest", "yellow")} =
+              prompt("Web Developer or Data Analyst");
+            </p>
+            <br />
+            <div style={{ marginLeft: "1rem" }}>
+              <span
+                style={{ marginLeft: "-1rem !important" }}
+                className="indent"
+              >
+                {textColor("let", "green")}
+                {" about = "}
+                {textColor(
+                  "`Passionate, motivated, life learner with experience in web development and data analysis. Seeking interesting and complex problems to solve.",
+                  "yellow"
+                )}
+                {";"}
+              </span>
+              <br />
+              <br />
+              <p>
+                {textColor("if", "green")} {"("}interest === "Web Developer"
+                {")"}
+                {"  {"}
+              </p>
+              <div style={{ marginLeft: "3rem" }}>
+                <span
+                  // style={{ marginLeft: "-1rem !important" }}
+                  className="indent"
+                >
+                  {"about = "}
+                  {textColor(
+                    `${"`"}I began learning HTML and CSS in 2017 while working as a Marketing Project Manager out of a desire to build landing pages.\n\nI quickly felt limited and found Javascript as the
+                  solution to set me free. In 2020, I decided to pursue web development and learned React. I have been building projects and continuing my education since.${"`"}`,
+                    "yellow"
+                  )}
+                  {";"}
+                </span>
+              </div>
+              <br />
+              <p>
+                {"} "}
+                {textColor("else if", "green")} {"("}interest === "Data Analyst"
+                {")"}
+                {"  {"}
+              </p>
+
+              <div style={{ marginLeft: "3rem" }}>
+                <span
+                  // style={{ marginLeft: "-1rem !important" }}
+                  className="indent"
+                >
+                  {"about = "}
+                  {textColor(
+                    `${"`"}I began learning HTML and CSS in 2017 while working as a Marketing Project Manager out of a desire to build landing pages.\n\nI quickly felt limited and found Javascript as the
+                  solution to set me free. In 2020, I decided to pursue web development and learned React. I have been building projects and continuing my education since.${"`"}`,
+                    "yellow"
+                  )}
+                  {";"}
+                </span>
+              </div>
+              {"}"}
+              <br />
+              <br />
+              <p>{textColor("return", "green")} about;</p>
+
+              <div style={{ marginLeft: "-2rem" }}>{"};"}</div>
+            </div>
           </div>
         </div>
       </div>
