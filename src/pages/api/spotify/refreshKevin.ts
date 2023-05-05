6bb68e1ccf26601e0a9487b8d9e1bcd21962a29f
Kevin Andrews <andrewskevin92@gmail.com>
2023-05-04 17:27:05 -0700

    fix createContext

--- src/pages/api/spotify/refreshKevin.ts
+++ src/pages/api/spotify/refreshKevin.ts
@@ -1,12 +1,12 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { appRouter } from "../../../server/trpc/router/_app";
-import { createTRPCContext } from "../../../server/trpc/context";
+import { createContext } from "../../../server/trpc/context";
 
 const refreshKevinHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
-  const ctx = await createTRPCContext({ req, res });
+  const ctx = await createContext({ req, res });
   const caller = appRouter.createCaller(ctx);
 
   // try {
